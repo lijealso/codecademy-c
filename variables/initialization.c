5f12ad337a5a4bc6a171c21703b1cc7460dadcb0
lijealso <lijealso@gmail.com>
2022-07-31 14:00:38 +0100

    variables add

---
+++ variables/initialization.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  
+  int numOfBooks = 9;
+  char favLetter = 'k';
+  char favDigit = '3';
+  double costOfCandyBar = 1.323;
+
+  printf("Number of books: %d\n", numOfBooks);
+  printf("Your Favorite Letter is: %c\n", favLetter);
+  printf("Your Favorite Digit is: %c\n", favDigit);
+  printf("You expect to pay $%.2f for a candy bar.\n", costOfCandyBar);
+}
