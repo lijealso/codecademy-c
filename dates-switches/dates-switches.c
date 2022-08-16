564efe4c8257975c7cf45b9011be9922deed2535
lijealso <lijealso@gmail.com>
2022-08-16 23:44:00 +0100

    project, dates and switches

--- dates-switches/dates-switches.c
+++ dates-switches/dates-switches.c
@@ -11,15 +11,69 @@ int main(void) {
 
   // Print the month
   
+  switch(month){
+    case 1:
+      printf("January ");
+      break;
+    case 2:
+      printf("February ");
+      break;
+    case 3:
+      printf("March ");
+      break;
+    case 4:
+      printf("April ");
+      break;
+    case 5:
+      printf("May ");
+      break;
+    case 6:
+      printf("June ");
+      break;
+    case 7:
+      printf("July ");
+      break;
+    case 8:
+      printf("August ");
+      break;
+    case 9:
+      printf("September ");
+      break;
+    case 10:
+      printf("October ");
+      break;
+    case 11:
+      printf("November ");
+      break;
+    case 12:
+      printf("December ");
+      break;
+  }
+
 
   // Print the day
 
+  printf("%d", day);
 
   // Print the suffix for a given day
   
-
+  switch(day)
+  {
+    case 1: case 21: case 31:
+      printf("st");
+      break;
+    case 2: case 22:
+      printf("nd");
+      break;
+    case 3: case 23:
+      printf("rd");
+      break;
+    default:
+      printf("th");
+  }
   // Print the year
   
+  printf(", 2022.\n");
   
   return 0;
 }
