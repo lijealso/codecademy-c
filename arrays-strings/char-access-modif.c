63d065801adcb26ba7010ef57ce3dcd9cc0c07f1
lijealso <lijealso@gmail.com>
2022-08-17 22:28:15 +0100

    arrays and strings

---
+++ arrays-strings/char-access-modif.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  char p[] = "procezzor";
+  // Checkpoint 1 code goes here
+
+  printf("%c\n", p[2]);
+
+  // Checkpoint 2 code goes here
+
+  p[5] = 's';
+  p[6] = 's';
+
+  printf("%s", p);
+
+}
