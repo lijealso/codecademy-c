4aadeb1436df8f4ceb6f76dc42e5e716687f94cf
lijealso <lijealso@gmail.com>
2022-08-17 22:29:09 +0100

    arrays and strings

---
+++ arrays-strings/length-array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+  double arr[] = {1.0, 8, 2.27, 16.2, 2, 3.14, 12.6, 1.62, 6.54, 9, 11};
+  
+  // Checkpoint 1 code goes here.
+  int len = sizeof(arr)/sizeof(double);
+
+  // Checkpoint 2 code goes here.
+  for (int i = 0 ; i < len ; i++)
+  {
+    printf("%f\n", arr[i]);
+  }
+}
