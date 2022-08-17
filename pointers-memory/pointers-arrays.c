184d5f5d196813184ed85e609b82c0f9e432e744
lijealso <lijealso@gmail.com>
2022-08-18 00:53:57 +0100

    pointers & memory

---
+++ pointers-memory/pointers-arrays.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    char s[] = "Farmer Jack realized that big yellow quilts were expensive!!";
+
+    // Code for Checkpoint 1 goes here.
+    int *ptr = &arr[9];
+
+    // Code for Checkpoint 2 goes here.
+    for (int i = 9; i >= 0; i--)
+    {
+        printf("%d", *ptr);
+        ptr--;
+    }
+
+    // Code for Checkpoint 3 goes here.
+    char *ptr2 = &s[0];
+
+    // Code for Checkpoint 4 goes here.
+    int len = strlen(s);
+    for (int i = 0; i < len; i++)
+    {
+        *ptr2 = '#';
+        ptr2++;
+    }
+    printf("%s\n", s);
+}
