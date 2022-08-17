23edc33474f54fcbad19541efcfd8e01c90cdb53
lijealso <lijealso@gmail.com>
2022-08-18 00:53:37 +0100

    pointers & memory

---
+++ pointers-memory/defer-operator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int k = 2000;
+    int *ptr = &k;
+
+    // Checkpoint 1 code goes here.
+    printf("%d\n", *ptr);
+
+    // Checkpoint 2 code goes here.
+    *ptr = 961;
+
+    printf("%i\n", k); // Notice how this variable changed value after Checkpoint 2!
+}
