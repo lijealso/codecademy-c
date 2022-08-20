ad0d7e86b6d044533ecfa1d93a2f263064bc6363
lijealso <lijealso@gmail.com>
2022-08-20 01:05:22 +0100

    functions

---
+++ functions/defining-functions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Write your code below
+void printMyFavoriteAnimal(void)
+{
+    printf("My favorite animal is a cat\n");
+}
+
+int main(void)
+{
+
+    printMyFavoriteAnimal();
+}
