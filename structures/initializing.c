4f917c8b1d2ce54b7dc6c5f863043ca3b77f8de9
lijealso <lijealso@gmail.com>
2022-08-20 01:07:19 +0100

    structures

---
+++ structures/initializing.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    struct Person
+    {
+        char firstName[25];
+        // Write your code below
+        char lastName[40];
+        int age;
+    };
+
+    // Write your code below
+    struct Person person1 = {"Ada", "Lovelace", 28};
+    struct Person person2 = {"Marie", "Curie", 44};
+
+    printf("%d\n", person2.age);
+}
